6787c3d2511dee48defdf1ed8159ecafdf1e5c42
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-03-11 01:02:14 -0300

    volume

--- app/src/main/java/com/example/androidmidias/MainActivity.java
+++ app/src/main/java/com/example/androidmidias/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.androidmidias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
+    private SeekBar seekVolume;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,25 +21,71 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+        inicializarSeekBar();
+    }
+
+    private void inicializarSeekBar() {
+        seekVolume = findViewById(R.id.seekVolume);
+        //audio manager
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        //recover volume max min
+        int volumeMaximo = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int volumeAtual = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        //seekbar
+        seekVolume.setMax(volumeMaximo);
+        seekVolume.setProgress(volumeAtual);
+        seekVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, i, AudioManager.FLAG_SHOW_UI);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     public void executarSom(View view) {
         if (mediaPlayer != null) {
             mediaPlayer.start();
         }
-
     }
 
-    public void pausarSom(View view){
-        if(mediaPlayer.isPlaying()){
+    public void pausarSom(View view) {
+        if (mediaPlayer.isPlaying()) {
             mediaPlayer.pause();
         }
     }
 
-    public void pararSom(View view){
-        if(mediaPlayer.isPlaying()){
+    public void pararSom(View view) {
+        if (mediaPlayer.isPlaying()) {
             mediaPlayer.stop();
             mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+        }
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(mediaPlayer != null && mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mediaPlayer.isPlaying()) {
+            mediaPlayer.pause();
         }
     }
 }
