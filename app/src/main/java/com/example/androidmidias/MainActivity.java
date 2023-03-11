bfd36fcae3a87275c5c21b055936aabf1ffeca44
Ricardo Ribeiro de Alvarenga <alvarenga.r.ricardo@gmail.com>
2023-03-11 00:24:21 -0300

    buttons

--- app/src/main/java/com/example/androidmidias/MainActivity.java
+++ app/src/main/java/com/example/androidmidias/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.androidmidias;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+    }
+
+    public void executarSom(View view) {
+        if (mediaPlayer != null) {
+            mediaPlayer.start();
+        }
+
+    }
+
+    public void pausarSom(View view){
+        if(mediaPlayer != null){
+            mediaPlayer.pause();
+        }
+    }
+
+    public void pararSom(View view){
+        if(mediaPlayer != null){
+            mediaPlayer.stop();
+        }
     }
 }
